7d9b019b77c14e50100ed6d4ade2d157cfa3c026
ShireCeil <jax.jiang.007@gmail.com>
2021-12-22 02:22:31 +0800

    add r86s-write-this-to-emmc

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # R86S-QuickInstaller
 R86S System Quick Installer
+
+# Need select package
+ <!-- - sgdisk: Fix GPT info
+ - partx: Refresh part info -->
+ - pciutils: Pci devices info
+ - usbutils: USB devices info
+ - igc: Intel I225 NIC Driver
+ - mlx4: CX341/CX342 NIC Driver
+
+sgdisk
--- r86s-quick-installer/src/r86s-write-this-to-emmc.sh
+++ r86s-quick-installer/src/r86s-write-this-to-emmc.sh
@@ -1,3 +1,4 @@
+echo Write This System To R86S EMMC
 echo WARNING!!!!!!
 echo Will Clean All Data On EMMC!!!
 echo 'Enter "confirm" to contine!:'
@@ -19,13 +20,30 @@ then
    echo "Not confirm...Cancel..."
    exit
 fi
-echo 'Do Clean EMMC....'
+
+echo "Umount exists emmc partition"
+# umount exists mmc mount
+for a_mount in `mount --list | grep /dev/mmc | awk '{print $1}'`
+do
+   umount $a_mount
+done
 # flush mmcblk0
+echo 'Do Clean EMMC....'
 (
     echo p
     echo g
     echo w
 ) | fdisk /dev/mmcblk0
+# write our data to emmc
+echo 'Writing Data...'
+# get devices of boot
+boot_device=`mount --list | grep /boot | awk 'NR==1{print $1}' | tr -d '0-9'`
+echo "Write $boot_device to /dev/mmcblk0"
+dd if=$boot_device of=/dev/mmcblk0 bs=1M count=200 oflag=direct
+(
+    echo w
+) | fdisk /dev/mmcblk0
+echo 'Done!'
 
 echo ''
 echo ''

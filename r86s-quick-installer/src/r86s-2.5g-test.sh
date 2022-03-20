816c98fab013181daab3077b222eb00f25d3bf5a
Jax Jiang <jax.jiang.007@gmail.com>
2022-03-20 23:43:37 +0800

    add 3windows for test

--- r86s-quick-installer/src/r86s-2.5g-test.sh
+++ r86s-quick-installer/src/r86s-2.5g-test.sh
@@ -30,4 +30,14 @@ tmux split-window -v
 echo 'cat /proc/cpuinfo | grep MHz' > /tmp/check_Mhz.sh
 tmux send-keys 'watch -n 1 sh /tmp/check_Mhz.sh'
 tmux send-keys enter
+echo 'while :
+do
+        trap "echo Exited!; exit;" SIGINT SIGTERM
+        iperf3 -c 192.168.60.1 --port 19999
+	iperf3 -c 192.168.60.1 --port 19999 -R
+done' > /tmp/3_speed.sh
+tmux selectp -t test-client.0
+tmux split-window -h
+tmux send-keys 'sh /tmp/3_speed.sh'
+tmux send-keys enter
 screen tmux -2 attach-session -d
